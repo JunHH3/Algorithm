a3a03f4584bb650b22bac239e88cabd992cc6b89
JunHH3 <168386492+JunHH3@users.noreply.github.com>
2024-05-18 00:33:26 +0900

    Update 시험 성적.js

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,3 +1,4 @@
+1번
 let fs = require("fs");
 let filePath =
   process.platform === "linux" ? "/dev/stdin" : "input.txt";
@@ -16,6 +17,30 @@ if (90 <= a && a <= 100) {
 } else if (70 <= a && a <= 79) {
   console.log("C");
 } else if (60 <= a && a <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
+
+2번
+let fs = require("fs");
+let filePath =
+  process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ");
+
+let score = parseInt(input);
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else if (score >= 60) {
   console.log("D");
 } else {
   console.log("F");
