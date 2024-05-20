8dd8305c6a42a5a1607d7ff0e4cabc5fc7255548
JunHH3 <168386492+JunHH3@users.noreply.github.com>
2024-05-20 23:06:10 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,19 @@
+let fs = require("fs");
+let filePath =
+  process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split(" ");
+
+const year = Number(input);
+
+if (
+  (year % 4 === 0 && year % 100 !== 0) ||
+  year % 400 === 0
+) {
+  console.log(1);
+} else {
+  console.log(0);
+}
