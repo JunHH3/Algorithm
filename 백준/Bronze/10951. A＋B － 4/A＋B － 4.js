9f89d8a69c7bc0b9ce6d3c80e294bddf7d4ed2f0
JunHH3 <168386492+JunHH3@users.noreply.github.com>
2024-05-23 22:52:49 +0900

    [Bronze V] Title: A+B - 4, Time: 164 ms, Memory: 10544 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+const { start } = require("repl");
+let filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+for (let i = 0; i < input.length; i++) {
+  let input1 = +input[i][0] + +input[i][2];
+
+  console.log(input1);
+}
