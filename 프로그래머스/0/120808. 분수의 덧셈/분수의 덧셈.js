2f400a9c1e6f1ab56cd0ed06d880663e8b7ee7da
JunHH3 <168386492+JunHH3@users.noreply.github.com>
2024-05-25 04:03:55 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -3,19 +3,13 @@ function gcd (a,b) {
 }
 
 function solution(numer1, denom1, numer2, denom2) {
-    const an4 = denom1 * denom2
+    const dnum = denom1 * denom2
     
-    const an1 = numer1 * denom2
-    const an2 = numer2 * denom1
+    const num = numer1 * denom2 + numer2 * denom1
     
-    const an3 = an1 + an2
+    const g = gcd(num, dnum)
     
-    const g = gcd(an3, an4)
-    
-    const an6 = an3 / g
-    const an7 = an4 / g
-    
-    return [an6, an7]
+    return [num/g, dnum/g]
     
 }
 
